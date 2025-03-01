2c972aa4b2cf27a59285e24bb4936e0d296f62f2
HimanshuUpadhyay01 <hu7131@gmail.com>
2025-03-02 00:42:41 +0530

    hello

---
+++ src/main/java/com/example/hellomessagingapp/controller/HelloRestController.java
@@ -0,0 +1,16 @@
+package com.example.hellomessagingapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloRestController {
+    @RequestMapping("/")
+    public String sayhello(){
+        return "Hello From BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+}
