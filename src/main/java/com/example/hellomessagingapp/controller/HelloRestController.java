e9fc3ed39308148622356150ec51b79a043c6620
HimanshuUpadhyay01 <hu7131@gmail.com>
2025-03-02 14:51:32 +0530

    hello with post

---
+++ src/main/java/com/example/hellomessagingapp/controller/HelloRestController.java
@@ -0,0 +1,27 @@
+package com.example.hellomessagingapp.controller;
+
+import com.example.hellomessagingapp.dto.UserDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloRestController {
+    @RequestMapping("/")
+    public String sayhello(){
+        return "Hello From BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    @GetMapping("/hello/param/{name}")
+    public String sayHelloWithPathVariable(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    @PostMapping("/post")
+    public String sayHelloWithPost(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+}
