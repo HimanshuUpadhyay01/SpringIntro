117ecc117658c9f5e7e216afdd5a0b44630f0a4f
HimanshuUpadhyay01 <hu7131@gmail.com>
2025-03-02 14:44:34 +0530

    hello with path

---
+++ src/main/java/com/example/hellomessagingapp/controller/HelloRestController.java
@@ -0,0 +1,21 @@
+package com.example.hellomessagingapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloRestController {
+    @RequestMapping("/")
+    public String sayhello(){
+        return "Hello From BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    @GetMapping("/hello/param/{name}")
+    public String sayHelloWithPathVariable(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+}
